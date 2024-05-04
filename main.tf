8f5a3c8b0b85c00f225aa797c0d09cf4a7c22b05
great-start <zhenya.bill@gmail.com>
2024-05-04 19:22:21 +0300

    tf_lesson

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
+terraform {
+  backend "gcs" {
+    bucket = "yev-bill-terraform-store"
+    prefix = "terraform/"
+  }
+}
+
 module "gke_cluster" {
-  source         = "github.com/${GOOGLE_PROJECT}/tf-google-gke-cluster"
+  source         = "github.com/great-start/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = 2
+  GKE_NUM_NODES  = var.GKE_NUM_NODES
 }
